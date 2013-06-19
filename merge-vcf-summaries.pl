0ed433c55df1a12132ae7db1f3879279511c9aec
Mike Covington <mfcovington@gmail.com>
2013-06-19 11:57:11 -0700

    fix samtools path and other cleanup

--- merge-vcf-summaries.pl
+++ merge-vcf-summaries.pl
@@ -12,13 +12,16 @@ use feature 'say';
 use List::Util 'max';
 use Capture::Tiny 'capture_stderr';
 use Parallel::ForkManager;
+use Data::Printer;
 
 # TODO:
 # - quality score cutoff??
+# - check samtools stderr for relevant error!
+# - getopts
 
 my @vcf_summary_files = @ARGV;
 
-my $verbose             = 0;
+my $verbose             = 1;
 my $replicate_count_min = 2;
 my $min_ratio           = 0.9;
 my $threads             = 3;
@@ -51,9 +54,6 @@ my %counts;
 $counts{merged}   += scalar keys $merged{$_}   for keys %merged;
 $counts{repeated} += scalar keys $repeated{$_} for keys %repeated;
 $counts{conflict} += scalar keys $conflict{$_} for keys %conflict;
-say "merged:   ", $counts{merged};
-say "repeated: ", $counts{repeated};
-say "conflict: ", $counts{conflict};
 
 for my $chr ( sort keys %merged ) {
     for my $pos ( sort { $a <=> $b } keys $merged{$chr}) {
@@ -65,27 +65,25 @@ for my $chr ( sort keys %merged ) {
 
 $counts{merged_filtered} += scalar keys $merged{$_} for keys %merged;
 
-say "merged (after conflict removal and repeat count filtering): ", $counts{merged_filtered};
-
-use Data::Printer;
-p %conflict;
-
-
+if ($verbose) {
+    say "merged:   ", $counts{merged};
+    say "repeated: ", $counts{repeated};
+    say "conflict: ", $counts{conflict};
+    say "merged (after conflict removal and repeat count filtering): ",
+      $counts{merged_filtered};
+    p %conflict;
+}
 
 my @chromosomes = qw( A01 A02 A03 A04 A05 A06 A07 A08 A09 A10 );
 my $pm = new Parallel::ForkManager($threads);
 for my $cur_chr ( @chromosomes ) {
     $pm->start and next;
 
-    my $mpileup_cmd = "/Users/mfc/installs/bin/samtools mpileup -r $cur_chr -f $ref_fa $par1_bam";
-    # my $mpileup_cmd = "/Users/mfc/installs/bin/samtools mpileup -r $cur_chr:$pos-$pos -f $ref_fa $par1_bam";
+    my $mpileup_cmd = "samtools mpileup -r $cur_chr -f $ref_fa $par1_bam";
     my $mpileup_fh;
     my $stderr = capture_stderr {    # suppress mpileup output sent to stderr
         open $mpileup_fh,   "-|", $mpileup_cmd;
     };
-
-    # my $mpileup_counter;
-
 
     open my $polydb_fh, ">", "polyDB.$cur_chr";
     say $polydb_fh join "\t", 'chr', 'pos', 'ref_base', 'snp_base', 'genotype', 'insert_position', 'SNP_CLASS';
@@ -95,82 +93,27 @@ for my $cur_chr ( @chromosomes ) {
         my ( $chr, $pos, $ref, $depth, $bases, $quals ) = split /\t/;
 
         next unless exists $merged{$chr}{$pos};
-        # $mpileup_counter++;
 
         my $alt = $merged{$chr}{$pos}{alt};
-
         my $alt_genotype = get_alt_genotype( $chr, $pos, $ref, $alt, $depth, $bases );
         next if $alt_genotype eq '';
-        # say "ALT: $alt_genotype";
-
-
         say $polydb_fh join "\t", $chr, $pos, $ref, $alt, $alt_genotype, 'NA', 'SNP';
-        # my $nogap_line = <$nogap_fh>;
-        # chomp( $gap_line, $nogap_line );
-        # my ( $chr, $pos, $gap_cov ) = split /\t/, $gap_line;
-        # my $nogap_cov = [ split /\t/, $nogap_line ]->[2];
-        # if ( exists $$cov_pos_ref{$chr}{$pos} ) {
-        #     $count++;
-        #     push @cov_data, [ $sample_id, $chr, $pos, $gap_cov, $nogap_cov ];
-        # }
-
-        # populate_and_reset( \$count, \@cov_data, \$schema ) if $count % 100000 == 0;
     }
     close $mpileup_fh;
     close $polydb_fh;
-
-    # say $mpileup_counter;
 
     $pm->finish;
 }
 $pm->wait_all_children;
 
-
-# for my $chr ( sort keys %merged ) {
-#     for my $pos ( sort { $a <=> $b } keys $merged{$chr}) {
-#     exit;
-#     }
-# }
-
-        # my $alt = $merged{$chr}{$pos}{alt};
-        # my $ref = $merged{$chr}{$pos}{ref};
-        # ref_or_alt( $chr, $pos, $alt );
-        # say $out_fh join "\t", $chr, $pos, $ref, $alt;
+exit;
 
 sub get_alt_genotype {
-
     my ( $chr, $pos, $ref, $alt, $depth, $bases ) = @_;
 
-#     my $par1_id  = "R500";
-#     my $par1_bam = "R500.good.bam";
-#     my $ref_fa   = "B.rapa_genome_sequence_0830.fa";
-
-#     my $line;
-#     capture_stderr {
-#         $line =
-# `/Users/mfc/installs/bin/samtools mpileup -r $chr:$pos-$pos -f $ref_fa $par1_bam`;
-#     };
-
-    # return if $line eq "";
-
-    # my ( $chr2, $pos2, $ref, $depth, $bases, $quals ) = split /\t/, $line;
-    my $skip_count  = count_skips($bases);
-    my $par1_alt_count   = count_base( $bases, $alt );
-    my $par1_ref_count   = count_base($bases);
-
-    if ($verbose) {
-        # print "\n$chr\t$pos\t$ref\t$depth\t$bases\t$quals";
-        print "\n$chr\t$pos\t$ref\t$depth\t$bases\n";#\t$quals";
-        say "ref count:  $par1_ref_count";
-        say "alt count:  $par1_alt_count";
-        say "skip count: $skip_count";
-        $depth = $depth - $skip_count;
-
-        if    ( $depth == 0 )                       { say "Insufficient coverage at $chr:$pos" }
-        elsif ( $par1_ref_count >= $min_ratio * $depth ) { say "$par1_id is $ref at $chr:$pos" }
-        elsif ( $par1_alt_count >= $min_ratio * $depth ) { say "$par1_id is $alt at $chr:$pos" }
-        else                                        { say "$par1_id is ambiguous at $chr:$pos" }
-    }
+    my $skip_count     = count_skips($bases);
+    my $par1_alt_count = count_base( $bases, $alt );
+    my $par1_ref_count = count_base($bases);
 
     my $alt_genotype;
     if    ( $depth == 0 )                            { $alt_genotype = '' }
@@ -202,33 +145,3 @@ sub count_base {
 
     return $count;
 }
-
-__END__
-
-$ time ./merge-vcf-summaries.pl A01.rep_*.E.var.flt.vcf.summary
-merged:   33149
-repeated: 33149
-conflict: 13
-merged (after conflict removal and repeat count filtering): 23252
-{
-    A01   {
-        5718644    1,
-        8030298    1,
-        10834898   1,
-        11050720   1,
-        13174070   1,
-        13531866   1,
-        21716037   1,
-        22878676   1,
-        24525675   1,
-        27153976   1,
-        27154874   1,
-        27269486   1,
-        27747204   1
-    }
-}
-
-real    0m0.615s
-user    0m0.510s
-sys 0m0.026s
-
